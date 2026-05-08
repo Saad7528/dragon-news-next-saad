1bcba97dd88d62026212c0887b39eed10fd54ce9
saad7528 <saad0174742@gmail.com>
2026-05-08 19:21:49 +0600

    homepage redirect to category/01 dynamically. reduce code

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,50 +1,9 @@
-import Image from "next/image";
-import LeftSideBar from "../component/homepage/news/LeftSideBar";
-import RightSideBar from "../component/homepage/news/RightSideBar";
-import News from "../component/homepage/news/News";
+import { redirect } from "next/navigation";
+
+const category_id = "01"
 
 
-const getCategories = async () => {
-  const res = await fetch('https://openapi.programming-hero.com/api/news/categories');
-  const data = await res.json();
-  return data.data.news_category;
-}
-
-const getNewsByCategoryId = async (category_id) => {
-  const res = await fetch(`https://openapi.programming-hero.com/api/news/category/${category_id}`);
-  const data = await res.json();
-  return data.data;
-
-}
 export default async function Home() {
-  const categories = await getCategories();
 
-  const news = await getNewsByCategoryId('02');
-  console.log(news);
-  return (
-
-    // Left Section
-    <div className="container mx-auto grid grid-cols-12 gap-4 my-10">
-      <div className="col-span-3">
-        <h2 className="text-3xl font-bold  ">All cat</h2>
-        <LeftSideBar categories={categories} activeId={"01"} />
-      </div>
-
-      {/* News Section   */}
-      <div className="col-span-6">
-        <h2 className="text-3xl font-bold bg-pink-500">All News</h2>
-      <div>
-        {
-          news.map(n => <News key={n._id} n={n}></News>)
-        }
-
-      </div>
-      </div>
-
-      {/* Right section */}
-      <div className="col-span-3">
-        <RightSideBar />
-      </div>
-    </div>
-  );
+  redirect(`/category/${category_id}`)
 }
