966f0383291529c23c0d613712de613fa99e945e
saad7528 <saad0174742@gmail.com>
2026-05-10 21:28:51 +0600

    add news details page show details by id

---
+++ src/app/(main)/category/[id]/loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NewsLoadingPage = () => {
+    return (
+        <div className='flex h-[85vh] items-center justify-center '>
+            Category News By Id Is Loading...
+            <span className="loading loading-dots loading-xl"></span>
+
+        </div>
+    );
+};
+
+export default NewsLoadingPage;
--- src/app/(main)/news/[id]/page.jsx
+++ src/app/(main)/news/[id]/page.jsx
@@ -1,6 +1,10 @@
+import { getNewsDetailsById } from '@/lib/data';
 import React from 'react';
 
-const NewsDetailsPage = () => {
+const NewsDetailsPage = async ({params}) => {
+    const {id} = await params;
+    const news = await getNewsDetailsById(id);
+    console.log("id -", news);
     return (
         <div>
             NewsDetailsPage
