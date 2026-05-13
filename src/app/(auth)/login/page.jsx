da32157dfd5618a5c49fc4e391452869e4991814
saad7528 <saad0174742@gmail.com>
2026-05-13 21:54:14 +0600

    log in page complete

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,15 +1,22 @@
 "use client"
 import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const LoginPage = () => {
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        console.log(email, password);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm()
 
+    console.log(errors);
+
+    
+    const handleSubmitFunc = (data) => {
+        console.log(data);
+       
     }
 
     return (
@@ -17,22 +24,25 @@ const LoginPage = () => {
             <div className='rounded-xl bg-slate-50 p-6 '>
                 <h2 className='text-3xl font-bold'>Login Your Account</h2>
 
-                <form onSubmit={handleSubmit}>
+                <form onSubmit={handleSubmit(handleSubmitFunc)}>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Your Email</legend>
-                        <input 
-                        type="text"
-                        name="email"
-                        className="input" 
-                        placeholder="Your Email" />
+                        <input
+                            type="text"
+                            {...register("email", { required: "Enter your Email" })}
+                            className="input"
+                            placeholder="Your Email" />
+                            {errors.email && <p className='text-red-600'>{errors.email.message}</p>}
                     </fieldset>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend">Your Password</legend>
-                        <input 
-                        type="text"
-                        name="password"
-                        className="input" 
-                        placeholder="Your Password" />
+                        <input
+                            type="text"
+                            {...register("password", { required: "Enter your Password" })}
+                            className="input"
+                            placeholder="Your Password" />
+                            {errors.password && <p className='text-red-600'>{errors.password.message}</p>}
+
                     </fieldset>
                     <button className='btn bg-slate-900 text-white font-bold w-full mt-4'>Login</button>
                     <p className='text-center mt-2'>Have no account? <Link href={'/register'} className='text-blue-900'>Register</Link></p>
