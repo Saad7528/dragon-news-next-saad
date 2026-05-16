cfe1de9901702fa77d7ece02ee95223acc693eec
saad7528 <saad0174742@gmail.com>
2026-05-17 01:28:17 +0600

    added gitHub authentication

--- src/app/component/homepage/news/RightSideBar.jsx
+++ src/app/component/homepage/news/RightSideBar.jsx
@@ -10,14 +10,19 @@ const RightSideBar = () => {
             provider: "google",
         });
     }
+    const handleGitHubSignin = async () => {
+        const data = await authClient.signIn.social({
+            provider: "github",
+        });
+    }
 
 
     return (
         <div>
             <h2 className="text-3xl font-bold mb-6">Login with</h2>
             <div className='flex flex-col gap-2'>
-                <button className='btn ' onClick={() => handleGoogleSignin()}><FaGoogle />Login With Google</button>
-                <button className='btn '><FaGithub />Login With Google</button>
+                <button className='btn ' onClick={handleGoogleSignin}><FaGoogle />Login With Google</button>
+                <button className='btn ' onClick={handleGitHubSignin}><FaGithub />Login With Google</button>
             </div>
 
         </div>
